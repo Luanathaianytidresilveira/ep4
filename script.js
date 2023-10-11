653507945ab2b20426250e7d1f10afc1fc95673e
Luanathaianytidresilveira <127975901+Luanathaianytidresilveira@users.noreply.github.com>
2023-10-11 08:49:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-function um(){
+function botao_resposta() {
+    var respostausuario1 = document.queryselector('input[name="resposta1"]: checked');
+    var respostausuario2 = document.queryselector('input[name="resposta2"]: checked');
+    var respostausuario3 = document.queryselector('input[name="resposta3"]: checked');
+    var respostausuario4 = document.queryselector('input[name="resposta4"]: checked');
+    var respostausuario5 = document.queryselector('input[name="resposta5"]: checked');
+    var respostausuario6 = document.queryselector('input[name="resposta6"]: checked');
+    var respostausuario7 = document.queryselector('input[name="resposta7"]: checked');
+    var respostausuario8 = document.queryselector('input[name="resposta8"]: checked');
+    var respostausuario9 = document.queryselector('input[name="resposta9"]: checked');
+    var respostausuario10 = document.queryselector('input[name="resposta10"]: checked');
 
-    alert("isso é um alerta!");
+
+    if(respostausuario1 && respostausuario2 && respostausuario3 && respostausuario4 && respostausuario5 && respostausuario6 && respostausuario7 && respostausuario8 && respostausuario9 && respostausuario10){
+        
+    }
+    
 }
-function dois(){
-
-    prompt("escreva seu nome:");
-}
-
